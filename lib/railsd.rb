63275d8273d4fa2ae74d0f728ae904f431df0951
John Nunemaker <nunemaker@gmail.com>
2013-04-17 16:08:50 -0400

    Convenience method to setup all the subscribers for a given client.

--- lib/railsd.rb
+++ lib/railsd.rb
@@ -1,7 +1,3 @@
-module Railsd
-  mattr_accessor :client
-end
-
 require "railsd/instrumentable"
 require "railsd/subscriber"
 require "railsd/subscribers/action_controller"
@@ -10,3 +6,23 @@ require "railsd/subscribers/action_mailer"
 require "railsd/subscribers/active_support"
 require "railsd/subscribers/active_record"
 require "railsd/subscribers/railsd"
+
+module Railsd
+  # Public: Shortcut method to setup all subscribers for a given client.
+  #
+  # client - The statsd instance that will receive all instrumentation.
+  #
+  # Returns Array of subscribers that were setup.
+  def self.subscribe(client)
+    subscribers = []
+
+    subscribers << Subscribers::ActionController.subscribe(client)
+    subscribers << Subscribers::ActionView.subscribe(client)
+    subscribers << Subscribers::ActionMailer.subscribe(client)
+    subscribers << Subscribers::ActiveSupport.subscribe(client)
+    subscribers << Subscribers::ActiveRecord.subscribe(client)
+    subscribers << Subscribers::Railsd.subscribe(client)
+
+    subscribers
+  end
+end
---
+++ test/railsd_test.rb
@@ -0,0 +1,20 @@
+require "helper"
+
+class RailsdTest < ActiveSupport::TestCase
+  test "subscribe" do
+    begin
+      subscribers = Railsd.subscribe(Statsd.new)
+      assert_instance_of Array, subscribers
+
+      subscribers.each do |subscriber|
+        assert_instance_of \
+          ActiveSupport::Notifications::Fanout::Subscriber,
+          subscriber
+      end
+    ensure
+      Array(subscribers).each do |subscriber|
+        ActiveSupport::Notifications.unsubscribe(subscriber)
+      end
+    end
+  end
+end
