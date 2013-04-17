aa53a8cd63e40513edf317105f765ea6e88d2296
John Nunemaker <nunemaker@gmail.com>
2013-04-17 14:55:29 -0400

    Helper assertions against the fake socket.

--- test/support/statsd_socket_test_helpers.rb
+++ test/support/statsd_socket_test_helpers.rb
@@ -16,4 +16,14 @@ module StatsdSocketTestHelpers
   def teardown_statsd_socket
     @statsd_socket = Thread.current[:statsd_socket] = nil
   end
+
+  def assert_timer(metric)
+    assert statsd_socket.timer?(metric),
+      "Expected the timer #{metric.inspect} to be included in #{statsd_socket.timer_metric_names.inspect}, but it was not."
+  end
+
+  def assert_counter(metric)
+    assert statsd_socket.counter?(metric),
+      "Expected the counter #{metric.inspect} to be included in #{statsd_socket.counter_metric_names.inspect}, but it was not."
+  end
 end
