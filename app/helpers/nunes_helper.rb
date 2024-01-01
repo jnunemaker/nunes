0d1a9256588c59369fad266e2dff0e8b825c2686
John Nunemaker <nunemaker@gmail.com>
2024-01-01 14:17:58 -0500

    Nicer display

---
+++ app/helpers/nunes_helper.rb
@@ -0,0 +1,13 @@
+module NunesHelper
+  def compact_time_ago(time)
+    if time.blank?
+      tag.span "None", class: "text-muted"
+    else
+      distance_of_time_in_words(
+        Time.now.utc,
+        time,
+        scope: "datetime.distance_in_words.compact_time_ago"
+      )
+    end
+  end
+end
