c6d9796b04e126ba9da72cbe028c46106ab22c2f
John Nunemaker <nunemaker@gmail.com>
2025-06-22 11:37:53 -0600

    Get rails and ruby on same version and running in dev/tests

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -14,5 +14,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "users#index"
 end
