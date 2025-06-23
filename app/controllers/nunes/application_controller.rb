b15468da3649730b6d18b42fbc02c982a0ac2b05
John Nunemaker <nunemaker@gmail.com>
2025-06-23 08:22:28 -0600

    Untrace nunes requests

--- app/controllers/nunes/application_controller.rb
+++ app/controllers/nunes/application_controller.rb
@@ -2,5 +2,12 @@
 
 module Nunes
   class ApplicationController < ActionController::Base
+    around_action :untrace_request
+
+    private
+
+    def untrace_request
+      Nunes.untraced { yield }
+    end
   end
 end
